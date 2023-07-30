47bdd8d2bff44fe848ac000e36e9a51e01ebacc0
unknown <grigorec749@gmail.com>
2023-07-29 19:42:31 -0700

    string

---
+++ string_if_elif.py
@@ -0,0 +1,19 @@
+# 1)***fie un semafor programat pt pietoni.la inceputul fiecarei ore,
+# timp de 3 minute este verde,dupa care timp de 2 minute este rosu
+# aceste ferestre de timp 3 respectiv 2 min continuand pe parcursul intregii ore.
+# se citeste de la tastatura un timp t(min)
+# sa se determine ce culoare are semaforul la timpul t.
+# t= 14(output rosu)
+
+# t = input('time:')
+# if int(t) % 5 >= 3:
+#     print('rosu')
+# else:print ('verde')
+
+# 2)Scrie un program care citeste un mesaj de la tastatura.daca mesajul este un nr
+# ridica l la puterea a doua si afiseaza rezultatul.daca nu este nr afiseaza mesajul pe ecran.
+# mesaj = input('introduceti un mesaj:')
+# if mesaj .isnumeric():
+#     print(int(mesaj)**2)
+# else:
+#     print(mesaj)
