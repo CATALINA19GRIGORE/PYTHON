d7d4e9931297ae6dd521b895499294fc86583e9e
unknown <grigorec749@gmail.com>
2023-07-29 17:19:20 -0700

    primul commit

---
+++ temesimple.py
@@ -0,0 +1,89 @@
+# 3) Introdu de la tastatura doua numere. Fa suma lor, diferenta  si produsul.
+# Afiseaza-le pe ecran.
+# a = int(input('nr:'))
+# b = int(input('nr2:'))
+# print(a+b)
+# print(a-b)
+# print(a*b)
+
+# 1)  Introdu de la tastatura un numar intreg. Printeaza pe ecran ‘PYTHON’
+# daca numarul este mai mare decat 10 si ‘python’ daca este mai mic decat 10.
+# nr = int(input('nr:'))
+# print(nr)
+#  if nr >10 :
+#      print("PYTHON")
+# if nr < 10:
+#     print("python")
+
+# 4)Numara caracterele unui text introdus de la tastatura folosind o bucla for.
+# text = input('textul este:')
+# for cuvant in text:
+#     if chr in cuvant :
+#         print(count(cuvant))
+
+
+# 2) Scrie un cuvant de la tastatura. Daca are mai putin de 5 caractere printeaza pe ecran ‘Cuvant scurt’,
+# daca are exact 5 caractere printeaza cuvantul introdus si
+# daca are mai mult de 8 caractere, printeaza ‘Cuvand lung’.
+
+# word = input('cuvant :')
+# if len(word) < 5:
+#     print('cuvant scurt')
+# if len(word)==5 :
+#     print ('word')
+# elif len(word)  > 8:
+#     print('cuvant lung')
+
+# 8)Pritneaza ;python’ pe ecran de 100 de ori
+# program = ("python ")
+# for python in program:
+#   print (program *100)
+
+# 10)Fa o lista cu 10 elemente. Sterge din lista ultimul elment.
+# lista = ['1','2','3','4','5','6','7','8','9','10']
+# print(lista.pop(-1))
+# print(lista)
+
+# 11)Se da o lista cu elementele cuvinte. Afiseaza o noua lista in care toate cuvintele din
+# lista initiala sunt scrise cu majuscule
+
+# lista = input("introduceti cuvintele:")
+# new_list = [lista.upper()]
+# print(new_list)
+
+# 15)Elimina dublicatele din lista l = [1, 2, 1, 3, 4, 3, 10]. Afiseasza lista fara dublicate.
+# lista = [1, 2, 1, 3, 4, 3, 10]
+# l = set(lista)
+# print(list(l))
+
+ # Se da urmatorul string.convertiti in snake_case.
+
+# name = 'first-name'
+# name = name.replace('-','_')
+
+#### introduceti o adresa de mail si parola####
+
+# user = 'python'
+# passw = 'python 2021'
+# username = input('introduceti user:')
+# password = input('indtroduceti parola:')
+# if username == user and password == passw :
+#     print('username si parola introduse corect')
+# else:
+#     print('username /parola introduse gresit')
+
+
+####numar par/pozitiv/negativ/####
+
+# number = int(input('introduceti nr:'))
+# if number < 0:
+#     print('numarul este negativ')
+# elif number == 0:
+#     print('numarul este 0')
+# elif number % 2 == 0:
+#     print('numarul este par')
+# else:
+#     print('numarul este impar')
+
+ #
+
